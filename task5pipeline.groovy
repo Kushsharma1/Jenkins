727649f89d0cf1125f037b79c53e0d03d73eea98
Kush Sharma <43674008+Kushsharma1@users.noreply.github.com>
2024-05-03 16:27:27 +1000

    Update task5pipeline.groovy

--- task5pipeline.groovy
+++ task5pipeline.groovy
@@ -36,6 +36,11 @@ pipeline {
                 echo "Performing code analysis using SonarQube"
             }
         }
+         stage('Code Improvement') {
+            steps {
+                echo "Adding a new stage for testing"
+            }
+        }
         stage('Security Scan') {
             steps {
                 echo "Performing security scan using OWASP Dependency-Check"
